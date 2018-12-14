fc57d5794593a714273068571cb2b72a0df1e68a
Peng <pengpeng2018@u.northwestern.edu>
2018-12-14 04:03:14 -0600

    modify setup_nite.bash to create symbolic link, thus make openni2_tracker can work with NiTE2

--- setup_nite.bash
+++ setup_nite.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
-ln -s ~/dev/NiTE-Linux-x64-2.2/Samples/Bin/NiTE2 ~/.ros/NiTE2
+# create a symbolic link to make openni2_tracker can work with Nite2
+# you should modify following line to make ~/.ros/NiTE2 direct to path_to_your_NiTE2_directory/Samples/Bin/NiTE2
+ln -s ~/src/NiTE-Linux-x64-2.2/Samples/Bin/NiTE2 ~/.ros/NiTE2
